0e46290ca2e6566d9292bc00f729eb28baef55d5
AlisonGu <agu@smith.edu>
2023-03-27 20:32:46 -0400

    input code

---
+++ R/Code for Input.R
@@ -0,0 +1,8 @@
+# Ask for user input of three cooking ingredients
+ingredient_1 <- readline(prompt = "Enter the first ingredient: ")
+ingredient_2 <- readline(prompt = "Enter the second ingredient: ")
+ingredient_3 <- readline(prompt = "Enter the third ingredient: ")
+
+# Print out the entered ingredients
+cat("You entered:", ingredient_1, ingredient_2, ingredient_3)
+
