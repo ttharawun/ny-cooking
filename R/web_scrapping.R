e6bf3706a0a438f87632176544ba189212e7b52f
Tint Tha Ra Wun <ttharawun@smith.edu>
2023-03-30 13:31:29 -0400

    add NULL check for all recipe parts

--- R/web_scrapping.R
+++ R/web_scrapping.R
@@ -5,66 +5,89 @@ recipes <- data.frame()
 
 #function to scrape the details of each recipe
 get_recipe<-function(url){
-  #store urls info into page
-  page <- rvest::read_html(url)
+      #store urls info into page
+      page <- rvest::read_html(url)
 
-  #scrape recipe title
-  recipe_title <- page |>
-    rvest::html_nodes(".header_recipe-name__RS14R") |>
-    rvest::html_text()
+      #scrape recipe title
+      recipe_title <- page |>
+        rvest::html_nodes(".header_recipe-name__RS14R") |>
+        rvest::html_text()
+      if (is.null(recipe_title)){
+        recipe_title <- ""
+      }
 
-  #scrape the recipe tags
-  recipe_tags <- page |>
-    rvest::html_nodes(".tags_tagListItem__EAD5e .link_default__XRQhR") |>
-    rvest::html_text()
-  recipe_tags <-paste0(recipe_tags, collapse = ",")
+      #scrape the recipe tags
+      recipe_tags <- page |>
+        rvest::html_nodes(".tags_tagListItem__EAD5e .link_default__XRQhR") |>
+        rvest::html_text()
+      recipe_tags <-paste0(recipe_tags, collapse = ",")
+      if (is.null(recipe_tags)){
+        recipe_tags <- ""
+      }
 
-  #scrape recipe serving number
-  recipe_yield <- page |>
-    rvest::html_nodes(".ingredients_recipeYield__Ljm9O") |>
-    rvest::html_text()
+      #scrape recipe serving number
+      recipe_yield <- page |>
+        rvest::html_nodes(".ingredients_recipeYield__Ljm9O") |>
+        rvest::html_text()
+      if (is.null(recipe_yield)){
+        recipe_yield <- ""
+      }
 
-  #scrape recipe ingredients
-  recipe_ingredients <- page |>
-    rvest::html_nodes(".ingredient_ingredient__lq70t span , .ingredientgroup_name__IZMKB") |>
-    rvest::html_text()
-  recipe_ingredients <- paste0(recipe_ingredients, collapse = "\n")
+      #scrape recipe ingredients
+      recipe_ingredients <- page |>
+        rvest::html_nodes(".ingredient_ingredient__lq70t span , .ingredientgroup_name__IZMKB") |>
+        rvest::html_text()
+      recipe_ingredients <- paste0(recipe_ingredients, collapse = "\n")
+      if (is.null(recipe_ingredients)){
+        recipe_ingredients <- ""
+      }
 
-  #scrape recipe instructions
-  recipe_instructions<- page |>
-    rvest::html_nodes(".editorialtext_editorialText__TGWwj , .preparation_stepNumber__cPykF") |>
-    rvest::html_text()
-  recipe_instructions <- paste0(recipe_instructions, collapse = "\n")
+      #scrape recipe instructions
+      recipe_instructions<- page |>
+        rvest::html_nodes(".editorialtext_editorialText__TGWwj , .preparation_stepNumber__cPykF") |>
+        rvest::html_text()
+      recipe_instructions <- paste0(recipe_instructions, collapse = "\n")
+      if (is.null(recipe_instructions)){
+        recipe_instructions <- ""
+      }
 
-  #scrape recipe rating
-  recipe_rating <- page |>
-    rvest::html_nodes(".stats_avgRating__DmjGC") |>
-    rvest::html_text()
+      #scrape recipe rating
+      recipe_rating <- page |>
+        rvest::html_nodes(".stats_avgRating__DmjGC") |>
+        rvest::html_text()
+      if (is.null(recipe_rating)){
+        recipe_rating <- ""
+      }
 
-  #scrape recipe preparation time
-  recipe_time <- page |>
-    rvest::html_node(".interfacecaptiontext_interfaceCaptionText__ymi7T:nth-child(2)") |>
-    rvest::html_text()
+      #scrape recipe preparation time
+      recipe_time <- page |>
+        rvest::html_node(".interfacecaptiontext_interfaceCaptionText__ymi7T:nth-child(2)") |>
+        rvest::html_text()
+      if (is.null(recipe_time)){
+        recipe_time <- ""
+      }
 
-  #scrape recipe comments number
-  recipe_comment_num <- page |>
-    rvest::html_node(".ratingssection_ratingsCount__q_DIQ") |>
-    rvest::html_text()
+      #scrape recipe comments number
+      recipe_comment_num <- page |>
+        rvest::html_node(".ratingssection_ratingsCount__q_DIQ") |>
+        rvest::html_text()
+      if (is.null(recipe_comment_num)){
+        recipe_comment_num <- ""
+      }
 
-  #combine all previous info to a dataframe
-  recipes <- data.frame(
-    title = recipe_title,
-    tag = recipe_tags,
-    serving = recipe_yield,
-    ingredients = recipe_ingredients,
-    rating = recipe_comment_num,
-    time = recipe_time,
-    instructions = recipe_instructions,
-    link = url
-  )
-
-  #return recipes as output
-  return(recipes)
+      #combine all previous info to a dataframe
+      recipes <- data.frame(
+        title = recipe_title,
+        tag = recipe_tags,
+        serving = recipe_yield,
+        ingredients = recipe_ingredients,
+        rating = recipe_comment_num,
+        time = recipe_time,
+        instructions = recipe_instructions,
+        link = url
+      )
+    #return recipes as output
+    return(recipes)
 }
 
 #scrape from NYT Cooking main page ----
@@ -90,7 +113,8 @@ for(i in 1:209){
   #scrape recipe link
   page_links <- page |>
     html_nodes("li div.recipecard_recipeCard__eY6sC article.atoms_card__sPaoj a.link_link__ov7e4") |>
-    html_attr('href')
+    html_attr('href') |>
+    Sys.sleep(10)
 
   #save into vector
   partial_page_links <- c(partial_page_links, page_links)
