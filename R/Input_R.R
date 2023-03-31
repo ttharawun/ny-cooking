ff740bf01c959f302a13fbb9cd2a4d295bc8af05
AlisonGu <agu@smith.edu>
2023-03-31 09:52:08 -0400

    add csv

--- R/Input_R.R
+++ R/Input_R.R
@@ -1,3 +1,7 @@
+
+# Import recipes
+NYT_recipes <- read.csv("R/NYT_recipes2.csv")
+
 # Ask for user input of three cooking ingredients
 ingredient_1 <- readline(prompt = "Enter the first ingredient: ")
 ingredient_2 <- readline(prompt = "Enter the second ingredient: ")
@@ -5,4 +9,7 @@ ingredient_3 <- readline(prompt = "Enter the third ingredient: ")
 
 # Print out the entered ingredients
 cat("You entered:", ingredient_1, ingredient_2, ingredient_3)
+
+
+NYT_selected
 
