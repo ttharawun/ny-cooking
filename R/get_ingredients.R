9fc906d47d5915b7d6baa9901f19d1462299eb0f
Zhen Nie <znie@smith.edu>
2023-04-05 01:36:51 -0400

    add typo checking

--- R/get_ingredients.R
+++ R/get_ingredients.R
@@ -1,19 +1,27 @@
 get_ingredients<-function(...){
+
+  #create ingredients vector
   ingredients <- vector(mode = "character", length = 3)
+
   for (i in 1:3) {
     attempts <- 0
+    # test if the input ingredient exists in all recipes
     repeat {
+
       ingredients[i] <- readline(prompt = paste0("Enter ingredient ", i, ": "))
+
       if (any(grepl(tolower(ingredients[i]), tolower(recipes$ingredients)))) {
         cat("\n", ingredients[i], "was found in the list of ingredients.")
         break
-      } else {
+      }
+      else {
         cat("\n", ingredients[i], "was NOT found in the list of ingredients.")
         attempts <- attempts + 1
         if (attempts == 3) {
           warning("Exceeded maximum number of attempts. Please restart the program.")
           stop()
-        } else {
+        }
+        else {
           user_choice <- readline(prompt = "Would you like to input a new ingredient? (Y/N)")
           if (user_choice == "Y") {
             next
@@ -23,6 +31,8 @@ get_ingredients<-function(...){
           }
         }
       }
+
+
     }
   }
 
@@ -31,5 +41,8 @@ get_ingredients<-function(...){
 
   # Print out the entered ingredients
   cat("You entered:", paste(ingredients, collapse = ", "))
+
+  # return ingredients as a vector
   return(ingredients)
+
 }
