91d5f6353eef0aa9b22245e6bb4777158af57852
Zhen Nie <znie@smith.edu>
2023-04-05 09:56:13 -0400

    delete typo check because it's covered

--- R/get_ingredients.R
+++ R/get_ingredients.R
@@ -7,19 +7,7 @@ get_ingredients<-function(...){
     attempts <- 0
 
     repeat {
-
-      # read user input
-      ingredient <- readline(prompt = paste0("Enter ingredient ", i, ": "))
-
-      # detect typo
-      if(!hunsbell::hunsbell_check(ingredient)){
-        warning("There is a typo in your input. Please restart the program.")
-        stop()
-      }
-      # store input into vector
-      ingredients[i] <- ingredient
-
-      # test if the input ingredient exists in all recipes
+      ingredients[i] <- readline(prompt = paste0("Enter ingredient ", i, ": "))
       if (any(grepl(tolower(ingredients[i]), tolower(recipes$ingredients)))) {
         cat("\n", ingredients[i], "was found in the list of ingredients.")
         break
@@ -27,25 +15,27 @@ get_ingredients<-function(...){
       else {
         cat("\n", ingredients[i], "was NOT found in the list of ingredients.")
         attempts <- attempts + 1
+
         if (attempts == 3) {
           warning("Exceeded maximum number of attempts. Please restart the program.")
           stop()
         }
-        # not working yet
         else {
           user_choice <- readline(prompt = "Would you like to input a new ingredient? (Y/N)")
           if (user_choice == "Y") {
             next
-          } else {
+          }
+
+          else {
             warning("Please restart the program and enter valid ingredients.")
             stop()
           }
+
         }
       }
-
-
     }
   }
+
 
   # Convert ingredients to lowercase
   ingredients <- tolower(ingredients)
