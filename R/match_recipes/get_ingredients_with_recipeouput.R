25e51fe6859a6f5da0bb3bcdef694fbd909d8934
Tint Tha Ra Wun <ttharawun@smith.edu>
2023-04-18 17:51:40 -0400

    Uncommented code

--- R/match_recipes/get_ingredients_with_recipeouput.R
+++ R/match_recipes/get_ingredients_with_recipeouput.R
@@ -1,69 +1,69 @@
-#' #' Title get ingredient with ouput
-#' #'
-#' #' @param ...
-#' #'
-#' #' @return a 10-row dataframe of recipes based on user input
-#' #' @export
-#' #'
-#' #' @examples get_ingredients()
-#' get_ingredients<-function(...){
-#'   recipes <- NYTrecipe
-#'   #create ingredients vector
-#'   ingredients <- vector(mode = "character")
-#'   indeces <- c()
-#'   userchoice = "Y"
-#'   i = 0
+#' Title get ingredient with ouput
 #'
-#'       repeat{
-#'       i = i + 1
+#' @param ...
 #'
-#'       ingredients[i] <- readline(prompt = paste0("Enter ingredient ", i, ": "))
-#'       word <- paste0("\\b",ingredients[i], "\\b")
+#' @return a 10-row dataframe of recipes based on user input
+#' @export
 #'
-#'       if(stringr::str_length(ingredients[i]) < 3){
-#'         print("Your word is too short. Please enter full word")
-#'         indeces[i] <- i
-#'       }
-#'
-#'       if(stringr::str_length(ingredients[i]) >= 3) {
-#'         if(hunspell::hunspell_check(ingredients[i])) {
-#'           if (any(grepl(tolower(word), tolower(recipes$ingredients)))) {
-#'             cat("\n", ingredients[i], "was found in the list of ingredients.")
-#'           }
-#'           else {
-#'             cat("\n", ingredients[i], "was NOT found in the list of ingredients.")
-#'             indeces[i] <- i
-#'           }
-#'         }
-#'         else {
-#'           cat("\n", ingredients[i], "is not correct. Please check your spelling.")
-#'           indeces[i] <- i
-#'         }
-#'       }
-#'
-#'       userchoice <- readline(prompt = "Would you like to input a new ingredient? (Y/N)")
-#'       if (userchoice == "Y") {next}
-#'       if (userchoice == "N") {next}
-#'       else {userchoice <- readline(prompt = "You typed something else than Y or N. Would you like to input a new ingredient? (Y/N)")}
-#'
-#'
-#'   }
-#'
-#'   # Convert ingredients to lowercase
-#'   ingredients <- tolower(ingredients)
-#'   ingredients <- ingredients[-indeces] #only include ingredients with correct spelling
-#'
-#'   # Print out the entered ingredients
-#'   cat("You entered:", paste(ingredients, collapse = ", "))
-#'
-#'   # call matching algorithm to get output
-#'   #source(here::here("R/matching_algorithm_ellipsis.R")) # change this later
-#'   recipes <- NewYorkTimesCooking::match_item(ingredients)
-#'
-#' }
-#'
-#'
-#' debugonce(get_ingredients)
-#'
-#'
-#' #get_ingredients()
+#' @examples get_ingredients()
+get_ingredients<-function(...){
+  recipes <- NYTrecipe
+  #create ingredients vector
+  ingredients <- vector(mode = "character")
+  indeces <- c()
+  userchoice = "Y"
+  i = 0
+
+      repeat{
+      i = i + 1
+
+      ingredients[i] <- readline(prompt = paste0("Enter ingredient ", i, ": "))
+      word <- paste0("\\b",ingredients[i], "\\b")
+
+      if(stringr::str_length(ingredients[i]) < 3){
+        print("Your word is too short. Please enter full word")
+        indeces[i] <- i
+      }
+
+      if(stringr::str_length(ingredients[i]) >= 3) {
+        if(hunspell::hunspell_check(ingredients[i])) {
+          if (any(grepl(tolower(word), tolower(recipes$ingredients)))) {
+            cat("\n", ingredients[i], "was found in the list of ingredients.")
+          }
+          else {
+            cat("\n", ingredients[i], "was NOT found in the list of ingredients.")
+            indeces[i] <- i
+          }
+        }
+        else {
+          cat("\n", ingredients[i], "is not correct. Please check your spelling.")
+          indeces[i] <- i
+        }
+      }
+
+      userchoice <- readline(prompt = "Would you like to input a new ingredient? (Y/N)")
+      if (userchoice == "Y") {next}
+      if (userchoice == "N") {next}
+      else {userchoice <- readline(prompt = "You typed something else than Y or N. Would you like to input a new ingredient? (Y/N)")}
+
+
+  }
+
+  # Convert ingredients to lowercase
+  ingredients <- tolower(ingredients)
+  ingredients <- ingredients[-indeces] #only include ingredients with correct spelling
+
+  # Print out the entered ingredients
+  cat("You entered:", paste(ingredients, collapse = ", "))
+
+  # call matching algorithm to get output
+  #source(here::here("R/matching_algorithm_ellipsis.R")) # change this later
+  recipes <- NewYorkTimesCooking::match_item(ingredients)
+
+}
+
+
+debugonce(get_ingredients)
+
+
+#get_ingredients()
