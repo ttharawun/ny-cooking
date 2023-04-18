1dce55aa255881717c95b9d74d5022e4413b69f7
Tint Tha Ra Wun <ttharawun@smith.edu>
2023-04-18 17:42:12 -0400

    clean get_ingredient Rscript and deleted unnecessary files

--- R/get_ingredients.R
+++ R/get_ingredients.R
@@ -1,52 +1,51 @@
+# Ask for user input of three cooking ingredients
+# Error message and stop the code if the attempt is over 3 times
 
-get_ingredients<-function(...){
-  #import recipe
-  recipes <- NewYorkTimesCooking::NYTrecipe
+get_ingredients <- function(...){
+  ingredients <- vector(mode = "character")
+  indeces <- c()
 
-  #create ingredients vector
-  ingredients <- vector(mode = "character", length = 3)
+  user_choice <- readline(prompt = "Would you like to enter an ingredient? (Y to continue or enter anything else to leave)")
+  i = 1
 
-  for (i in 1:3) {
+  while (user_choice == "Y") {
     attempts <- 0
+    ingredients[i] <- readline(prompt = paste0("Enter ingredient ", i, ": "))
 
-    repeat {
-      ingredients[i] <- readline(prompt = paste0("Enter ingredient ", i, ": "))
-      if (any(grepl(tolower(ingredients[i]), tolower(recipes$ingredients)))) {
-        cat("\n", ingredients[i], "was found in the list of ingredients.")
-        break
-      }
-      else {
-        cat("\n", ingredients[i], "was NOT found in the list of ingredients.")
-        attempts <- attempts + 1
+    word <- paste0("\\b",ingredients[i], "\\b")
 
-        if (attempts == 3) {
-          warning("Exceeded maximum number of attempts. Please restart the program.")
-          stop()
+    if(stringr::str_length(ingredients[i]) < 3){
+      print("Your word is too short. Please enter full word")
+      indeces[i] <- i
+    }
+
+    if(stringr::str_length(ingredients[i]) >= 3) {
+      if(hunspell::hunspell_check(ingredients[i])) {
+        if (any(grepl(tolower(word), tolower(NYTrecipe$ingredients)))) {
+          cat("\n", ingredients[i], "was found in the list of ingredients.")
         }
         else {
-          user_choice <- readline(prompt = "Would you like to input a new ingredient? (Y/N)")
-          if (user_choice == "Y") {
-            next
-          }
-
-          else {
-            warning("Please restart the program and enter valid ingredients.")
-            stop()
-          }
-
+          cat("\n", ingredients[i], "was NOT found in the list of ingredients.")
+          indeces[i] <- i
         }
       }
+      else {
+        cat("\n", ingredients[i], "is not correct. Please check your spelling.")
+        indeces[i] <- i
+      }
     }
+
+    userchoice <- readline(prompt = "Would you like to input a new ingredient? (Y/N)")
+    if (userchoice == "Y") {next}
+
+    # Convert ingredients to lowercase
+    ingredients <- tolower(ingredients)
+    ingredients <- ingredients[-indeces] #only include ingredients with correct spelling
+
+    # Print out the entered ingredients
+    cat("You entered:", paste(ingredients, collapse = ", "))
   }
-
-
-  # Convert ingredients to lowercase
-  ingredients <- tolower(ingredients)
-
-  # Print out the entered ingredients
-  cat("You entered:", paste(ingredients, collapse = ", "))
-
-  # return ingredients as a vector
-  return(ingredients)
-
 }
+
+
+
