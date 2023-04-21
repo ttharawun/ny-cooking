1304ad949338e64e84da7e72c005035bf8342a65
Tint Tha Ra Wun <ttharawun@smith.edu>
2023-04-21 15:14:10 -0400

    license and dataset documentation (in progress)

---
+++ data-raw/-data-raw-NYTrecipe-csv.R
@@ -0,0 +1,19 @@
+## code to prepare `./data-raw/NYTrecipe.csv` dataset goes here
+
+usethis::use_data(./data-raw/NYTrecipe.csv, overwrite = TRUE)
+
+NYTrecipe$ingredients <- textclean::replace_non_ascii(NYTrecipe$ingredients, replacement = "", remove.nonconverted = TRUE)
+
+NYTrecipe$instructions <- textclean::replace_non_ascii(NYTrecipe$instructions, replacement = "", remove.nonconverted = TRUE)
+
+NYTrecipe$title <- textclean::replace_non_ascii(NYTrecipe$title, replacement = "", remove.nonconverted = TRUE)
+
+NYTrecipe$tag <- textclean::replace_non_ascii(NYTrecipe$tag, replacement = "", remove.nonconverted = TRUE)
+
+NYTrecipe$time <- textclean::replace_non_ascii(NYTrecipe$time, replacement = "", remove.nonconverted = TRUE)
+
+NYTrecipe$serving <- textclean::replace_non_ascii(NYTrecipe$time, replacement = "", remove.nonconverted = TRUE)
+
+NYTrecipe$rating <- textclean::replace_non_ascii(NYTrecipe$rating, replacement = "", remove.nonconverted = TRUE)
+
+usethis::use_data(NYTrecipe, overwrite = TRUE)
