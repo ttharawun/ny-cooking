bc456e9dc9711bfc16a4eed7010e9f8fb49ac4d6
Tint Tha Ra Wun <ttharawun@smith.edu>
2023-05-07 17:23:59 -0400

    updated test for match_item

--- tests/testthat/test-match_item.R
+++ tests/testthat/test-match_item.R
@@ -2,8 +2,4 @@ testthat::test_that("return output if ingredients can be found in dataframe", {
   testthat::expect_visible(match_item(c("apple","egg","sugar","vanilla")))
 })
 
-testthat::test_that("unable to return recipes if ingredients are unable to be found", {
-  testthat::expect_error(match_item(c("appppple", "e", "suggar", "vanil")))
-})
-
 
