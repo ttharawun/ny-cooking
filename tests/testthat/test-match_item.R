0f446fff4f33824d825da8c19ee110c1a8c8e348
Teddy Martin <tkmartin@smith.edu>
2023-05-07 14:42:10 -0400

    fixing messages

--- tests/testthat/test-match_item.R
+++ tests/testthat/test-match_item.R
@@ -1,8 +1,8 @@
-testthat::test_that("print messages if all correct ingredients", {
-  testthat::expect_message(match_item(c("apple","egg","sugar","vanilla")))
+testthat::test_that("return output if ingredients can be found in dataframe", {
+  testthat::expect_visible(match_item(c("apple","egg","sugar","vanilla")))
 })
 
-testthat::test_that("unable to return", {
+testthat::test_that("unable to return recipes if ingredients are unable to be found", {
   testthat::expect_error(match_item(c("appppple", "e", "suggar", "vanil")))
 })
 
